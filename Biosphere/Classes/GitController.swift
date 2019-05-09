afa77799ec4ba08316f29c790fcad21b77c28c72
halo <github@posteo.org>
2019-05-09 19:43:55 +0200

    Catch executable errors

--- Biosphere/Classes/GitController.swift
+++ Biosphere/Classes/GitController.swift
@@ -1,15 +1,11 @@
 import Cocoa
 
 class GitController: NSWindowController {
- // @IBOutlet var installXcodeToolsButton: NSButton?
-  
   public var isSatisfied: Bool {
     return false
   }
-
   
   @IBAction func installCommandLineTools(sender: NSButton) {
-    Log.debug("do it")
     let task = Process()
     task.executableURL = URL(fileURLWithPath: "/usr/bin/xcode-select")
     task.arguments = ["--install"]
@@ -19,33 +15,19 @@ class GitController: NSWindowController {
     task.standardError = pipe
 
     task.terminationHandler = { (process) in
-      Log.debug("\ndidFinish: \(!process.isRunning)")
-      Log.debug("status: \(process.terminationStatus)")
+      Log.debug("xcode-select exitet with status: \(process.terminationStatus)")
+      
       let data = pipe.fileHandleForReading.readDataToEndOfFile()
-      if let string = String(data: data, encoding: String.Encoding.utf8) {
-        Log.debug(string)
+      if let output = String(data: data, encoding: String.Encoding.utf8) {
+        Log.debug(output)
       }
     }
+    
     do {
       try task.run()
-    } catch {}
-    /*
-    let handle = pipe.fileHandleForReading
-    handle.waitForDataInBackgroundAndNotify()
-    
-    
-    // When new data is available
-    var dataAvailable : NSObjectProtocol!
-    dataAvailable = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable,
-                                                           object: handle, queue: nil) { notification -> Void in
-    let data = pipe.fileHandleForReading.availableData
-    if data.count > 0 {
-      if let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
-        Log.debug(str as String)
-      }
+    } catch {
+      Log.debug("Failed to execute xcode-select. Does the executable exist?")
     }
-    }
- */
   }
   
   override var windowNibName: String! {
